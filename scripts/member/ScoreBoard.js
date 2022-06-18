f03ed5526d514274d19b63900ee156cb4283d806
yessinetrigui <yessinetrigui03@gmail.com>
2022-06-18 14:06:06 +0100

    Add Search Engin For ScoreBoard[Organizer]

--- scripts/member/ScoreBoard.js
+++ scripts/member/ScoreBoard.js
@@ -1,12 +1,24 @@
-function showPopUp(id){
+function showPopUp(id, rank, name, points){
     document.getElementById("popUp").style.display = "flex";
-    document.getElementById("title").innerHTML = document.getElementById("r"+id).innerHTML ;
-    document.getElementById("name").innerHTML = document.getElementById("n"+id).innerHTML ;
-    document.getElementById("score").innerHTML = document.getElementById("s"+id).innerHTML ;
+    document.getElementById("title").innerHTML =  "#"+rank;
+    document.getElementById("name").innerHTML = name ;
+    document.getElementById("score").innerHTML = points ;
 
 }
 function HidePopUp(){
     document.getElementById("popUp").style.display = "none";
 }
 
+function updateView(){
+    searchwords = document.getElementById('searchwords').value;
+    var container_div = document.getElementById('views')
+    for(i=1; i<=container_div.getElementsByTagName('div').length; i++){
+        if(searchwords==""){
+            document.getElementById('lab'+i).style.display = "block"
+        }
+        if(document.getElementById('n'+i).innerHTML.toUpperCase().indexOf(searchwords.toUpperCase())==-1){
+            document.getElementById('lab'+i).style.display = "none"
+        }
+    }
+}
 
