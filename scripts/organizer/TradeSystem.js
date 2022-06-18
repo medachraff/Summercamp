5c29e6ef8540ab72164caed810138a982495aa7b
medachraff <achrafbensaad@icloud.com>
2022-06-18 22:21:29 +0100

    boom

--- scripts/organizer/TradeSystem.js
+++ scripts/organizer/TradeSystem.js
@@ -140,3 +140,12 @@ function Done(QrNum){
     CloseQr()
     document.getElementById('Btn_'+QrNum).innerHTML = "ScanQr_"+QrNum
 }
+function onScanSuccess(decodedText, decodedResult) {
+    console.log(`Code scanned = ${decodedText}`, decodedResult);
+    document.getElementById('QrCodeTXT').value = decodedText;
+    document.getElementById('qr-reader').style.display = 'none';
+    document.getElementById('titleQRdONE').style.display = 'block';
+}
+var html5QrcodeScanner = new Html5QrcodeScanner(
+ "qr-reader", { fps: 10, qrbox: 250 });
+html5QrcodeScanner.render(onScanSuccess);
