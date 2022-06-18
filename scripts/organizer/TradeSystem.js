e8a4e820f51b427d7892ba968544be2af1c3e3b5
yessinetrigui <yessinetrigui03@gmail.com>
2022-06-18 07:58:49 +0100

    Update TRADE SYSTEM [JS]

--- scripts/organizer/TradeSystem.js
+++ scripts/organizer/TradeSystem.js
@@ -1,123 +1,114 @@
-
 /******* W A R N I N G  ****/
 /***** DO NOT TOUCH THIS FILE *****/
-
-function AddCount(){
-    document.getElementById("COUNTER").innerHTML = Number(document.getElementById("COUNTER").innerHTML)+1;
-}
-
-function MinCount(){
-    x = Number(document.getElementById("COUNTER").innerHTML)-1;
-    if(x<0){
-    document.getElementById("COUNTER").innerHTML = 0;
-    }else{
-        document.getElementById("COUNTER").innerHTML = x;
+class TradeSys{
+    constructor(InputNumber){
+        this.InputNumber = InputNumber;
+        this.typeTrade = "none"
+        this.Count = 0
+        this.IdCard = "unset"
+        this.CardName = "Search for...."
+        this.Coins = "Enter the budget"
     }
-}
-function HideAllPopUp(n){
-    /* to get back the counter to 0 **/
-    document.getElementById("COUNTER").innerHTML = 0;
-    /* to get back the coins to 0 **/
-    document.getElementById("coins").value = "";
-    if(n==1){
-        /* to get back the items of main page **/
-        document.getElementById("header").style.display = "flex";
-        document.getElementById("main").style.display = "block";
-        document.getElementById("footer").style.display = "flex";
-        document.getElementById("popUp").style.backgroundColor = "rgba(0, 0, 0, 0.729)";
-        document.getElementById("popUp").style.position = "fixed";
+    element(id){
+        return document.getElementById(id)
     }
-    PopUpList = ["popUp", "itemToTrade", "CoinPopUP", "CardsPopUP", "SearchCardsPopUP"]
-    for(i=0; i<PopUpList.length; i++){
-        document.getElementById(PopUpList[i]).style.display = "none";
-    }
-}
-function showPopUp(id, Hid){
-    if(Hid!=undefined){
-        document.getElementById("DT").setAttribute("value", Hid);
-
-    }
-    HideAllPopUp(0)
-    if(id!="SearchCardsPopUP"){
-        document.getElementById("popUp").style.display = "flex";
-        document.getElementById(id).style.display = "flex";
-        document.getElementById(id).classList.add = "test";
-        if(id=="itemToTrade"){
-            document.getElementById("Btn_To_Coin").setAttribute("onclick", "showPopUp('CoinPopUP',"+Hid+")");
-            document.getElementById("Btn_To_Card").setAttribute("onclick", "showPopUp('CardsPopUP',"+Hid+")");
-        }
-        if(id=="CoinPopUP"){
-            document.getElementById("Btn_Set_Coins").setAttribute("onclick", "Added('',document.getElementById('coins').value+' Coin',"+Hid+",'',document.getElementById('coins').value)");
-        }
-    }else{
-        document.getElementById("header").style.display = "none";
-        document.getElementById("main").style.display = "none";
-        document.getElementById("footer").style.display = "none";
-        document.getElementById("popUp").style.display = "flex";
-        document.getElementById("popUp").style.backgroundColor = "unset";
-        document.getElementById("popUp").style.position = "unset";
-        document.getElementById(id).style.position = "relative";
-        document.getElementById(id).style.display = "flex";
-    }
-}
-function Added(idCard, name, ItemNumber, NumberCards, NumberCoin){
-    HideAllPopUp()
-   
-    
-    if(ItemNumber==1){
-        document.getElementById('FI_TypeItem').value="unset";
-        document.getElementById('FI_Coins').value ="unset";
-        document.getElementById('FI_IdCard').value="unset";
-        document.getElementById('FI_CardNumber').value="unset";
-        if(name.indexOf("Coin")!=-1){
-            document.getElementById('first_item').value = name;
-            document.getElementById('FI_Coins').value = NumberCoin;
-            document.getElementById('FI_TypeItem').value = "Coin";
+    showPopUp(PopUpName, para="none"){
+        //HideAllPopUp("unset")
+        if(PopUpName!="SearchCardsPopUP"){
+            this.element("popUp").style.display = "flex";
+            this.element(PopUpName).style.display = "flex";
+            this.element("COUNTER").innerHTML = this.Count;
+            this.element("txt_card").value = this.CardName;
+            if(para!="no-defaultCC"){
+                this.element("txt_card").style.color = "#D2D2D2";
+                this.element("coins").style.color = "#D2D2D2";
+            }
+            this.element("coins").value = this.Coins;
         }else{
-            document.getElementById('first_item').value = name+" x"+NumberCards;
-            document.getElementById('FI_TypeItem').value = "Card";
-            document.getElementById('FI_IdCard').value = idCard;
-            document.getElementById('FI_CardNumber').value = NumberCards;
+            this.element("header").style.display = "none";
+            this.element("main").style.display = "none";
+            this.element("footer").style.display = "none";
+            this.element("popUp").style.display = "flex";
+            this.element("popUp").style.backgroundColor = "unset";
+            this.element("popUp").style.position = "unset";
+            this.element(PopUpName).style.position = "relative";
+            this.element(PopUpName).style.display = "flex";
         }
-        document.getElementById('first_item').style.color ="black";
-    }else if(ItemNumber==2){
-        document.getElementById('SI_TypeItem').value="unset";
-        document.getElementById('SI_Coins').value ="unset";
-        document.getElementById('SI_IdCard').value="unset";
-        document.getElementById('SI_CardNumber').value="unset";
-        if(name.indexOf("Coin")!=-1){
-            document.getElementById('second_item').value = name;
-
-            document.getElementById('SI_Coins').value = NumberCoin;
-            document.getElementById('SI_TypeItem').value = "Coin";
-        }else{
-            document.getElementById('second_item').value = name+" x"+NumberCards;
-
-            document.getElementById('SI_TypeItem').value = "Card";
-            document.getElementById('SI_IdCard').value = idCard;
-            document.getElementById('SI_CardNumber').value = NumberCards;
-        }
-        document.getElementById('second_item').style.color ="black";
-    }   
-}
-function SelectCard(id, name){
-    HideAllPopUp(1)
-    showPopUp('CardsPopUP')
-    document.getElementById("txt").type="text";
-    document.getElementById("txt").value=name;
-    document.getElementById("txt").readOnly=true;
-    document.getElementById("id_Item").value = id;
-}
-function DoneCard(id){
-    Count =  document.getElementById("COUNTER").innerHTML;
-    if(Count!=0){
-        ItemNumber =document.getElementById("DT").getAttribute("value");
-        name= document.getElementById("txt").value;
-        Added(id,name, ItemNumber, Count, 0)
-        document.getElementById("txt").type="text";
-        document.getElementById("txt").value="Select Card";
-        document.getElementById("txt").readOnly=true;
     }
-   
-}
+    HideAllPopUp(para='unset'){
+        if(para=="Reset"){
+            /* to get back the items of main page **/
+            this.element("header").style.display = "flex";
+            this.element("main").style.display = "block";
+            this.element("footer").style.display = "flex";
+            this.element("popUp").style.backgroundColor = "rgba(0, 0, 0, 0.729)";
+            this.element("popUp").style.position = "fixed";
+        }
+        this.PopUpList = ["popUp", "itemToTrade", "CoinPopUP", "CardsPopUP", "SearchCardsPopUP"]
+        for(this.i=0; this.i<this.PopUpList.length; this.i++){
+            this.element(this.PopUpList[this.i]).style.display = "none";
+        }
+    }
+    setCoin(){
+        if(this.InputNumber==1){
+            this.InputId = "first_item"
+        }else{
+            this.InputId = "second_item"
+        }
+        this.Coins = this.element("coins").value
+        this.element(this.InputId).value= this.Coins+" Coins";
+        this.element(this.InputId).style.color = "black";
+        this.HideAllPopUp('none')
+        this.typeTrade = "Coins"
+        this.FillHiddenWithDataLiSidkHoussem()
+    }
 
+    addCardsNumber(){
+        this.Count = Number(this.element("COUNTER").innerHTML)+1
+        this.element("COUNTER").innerHTML = this.Count;
+    }
+    minCardsNumber(){
+        this.Count = Number(this.element("COUNTER").innerHTML)-1;
+        if(this.Count<0){
+            this.Count = 0   
+        }
+        this.element("COUNTER").innerHTML = this.Count;
+    }
+    selectCard(IdCard, CardName){
+        this.CardName = CardName;
+        this.element("txt_card").value = this.CardName;
+        this.element("txt_card").style.color ="black";
+        this.HideAllPopUp('Reset')
+        this.showPopUp('CardsPopUP',"no-defaultCC")
+        this.IdCard = IdCard
+    }
+    setCard(){
+        this.HideAllPopUp()
+        if(this.InputNumber==1){
+            this.InputId = "first_item"
+        }else{
+            this.InputId = "second_item"
+        }
+        this.element(this.InputId).value= this.CardName+ " x"+ this.Count;
+        this.element(this.InputId).style.color= "black";
+        this.typeTrade = "Cards"
+        this.FillHiddenWithDataLiSidkHoussem()
+    }
+    FillHiddenWithDataLiSidkHoussem(){
+        if (this.InputNumber==1){
+            this.pfx="FI"
+        }else{
+            this.pfx="SI"
+        }
+        this.element(this.pfx+"_Coins").value = " ";
+        this.element(this.pfx+"_IdCard").value = " ";
+        this.element(this.pfx+"_CardNumber").value = " ";
+        this.element(this.pfx+"_TypeItem").value = this.typeTrade
+        if(this.typeTrade=="Coins"){
+            this.element(this.pfx+"_Coins").value = this.Coins
+        }else{
+            this.element(this.pfx+"_IdCard").value = this.IdCard
+            this.element(this.pfx+"_CardNumber").value = this.Count
+        }
+    }
+}
