8e2e09b60a5b4c65f2097abf529163d81734f298
yessinetrigui <yessinetrigui03@gmail.com>
2022-06-18 21:11:28 +0100

    Fix QrCode RegisterPage

--- scripts/Guest/registerpage.js
+++ scripts/Guest/registerpage.js
@@ -1,5 +1,8 @@
 function onScanSuccess(decodedText, decodedResult) {
     console.log(`Code scanned = ${decodedText}`, decodedResult);
+    document.getElementById('QrCodeTXT').value = decodedText;
+    document.getElementById('qr-reader').style.display = 'none';
+    document.getElementById('titleQRdONE').style.display = 'block';
 }
 var html5QrcodeScanner = new Html5QrcodeScanner(
  "qr-reader", { fps: 10, qrbox: 250 });
